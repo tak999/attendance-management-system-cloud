a3e1b7e1de6f89745b7f796b01c23f3c125d6401
Takuto Kinjo <takuto.takuto.124@gmail.com>
2023-01-24 22:35:50 +0900

    add: サンプルファイルを追加

--- supabase-chat/src/App.vue
+++ supabase-chat/src/App.vue
@@ -1,18 +1,53 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- form を submit すると sendMessage が呼ばれます -->
+    <form @submit.prevent="sendMessage">
+      <!-- 名前とメッセージの各 input タグの入力値は、 それぞれ inputName, inputMessage と同期されます -->
+      <input type="text" v-model="inputName" placeholder="名前" /><br />
+      <input type="text" v-model="inputMessage" placeholder="メッセージ" /><br />
+      <button type="submit">送信</button>
+    </form>
+    <ul>
+      <!-- messages に入っているメッセージをループして出力します -->
+      <li v-for="message in messages" :key="message.id">
+        <!-- message はオブジェクトで、 name, message, id, ここにはないですが createdAt 属性があることを期待しています -->
+        <b>{{ message.name }}</b> {{ message.message }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: "App",
+  data() {
+    return {
+      messages: [],
+      inputName: "",
+      inputMessage: "",
+    };
+  },
+  methods: {
+    sendMessage() {
+      // TODO: メッセージを送信
+    },
+    getMessages() {
+      // TODO: メッセージを取得
+    },
+    subscribeMessage() {
+      // TODO: メッセージを購読
+    },
+    unsubscribeMessage() {
+      // TODO: メッセージを購読をやめる
+    },
+  },
+  mounted() {
+    this.getMessages()
+    this.subscribeMessage()
+  },
+  beforeDestroy() {
+    this.unsubscribeMessage()
+  },
 }
 </script>
 
